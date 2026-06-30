21d078b61cd0438d689cba1d363ab7dbe3ab9f85
bobb.1 <137739660+bobbbb1@users.noreply.github.com>
2026-06-30 23:41:21 +0700

    Update protocol.py

--- devel/core/protocol.py
+++ devel/core/protocol.py
@@ -1,5 +1,38 @@
-from cyclonedds.domain import DomainParticipant
-from cyclonedds.idl import IdlStruct
-from cyclonedds.topic import Topic
-from cyclonedds.sub import Subscriber as DDSSubscriber, DataReader
-from cyclonedds.pub import Publisher as DDSPublisher, DataWriter
+# from cyclonedds.domain import DomainParticipant
+# from cyclonedds.idl import IdlStruct
+# from cyclonedds.topic import Topic
+# from cyclonedds.sub import Subscriber as DDSSubscriber, DataReader
+# from cyclonedds.pub import Publisher as DDSPublisher, DataWriter
+
+import os
+import sys
+
+# ========================================================
+# --- BAGIAN INISIALISASI (Jangan Diubah, Sudah Sempurna) ---
+# ========================================================
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+c_install_path = os.path.abspath(os.path.join(current_dir, "..", "lib", "rovosds", "install"))
+os.environ["CYCLONEDDS_HOME"] = c_install_path
+
+dll_path = os.path.join(c_install_path, "bin")
+if os.path.exists(dll_path):
+    os.add_dll_directory(dll_path)
+
+python_src_path = os.path.abspath(os.path.join(current_dir, "..", "lib", "rovosds", "cyclonedds-python"))
+if python_src_path not in sys.path:
+    sys.path.insert(0, python_src_path)
+
+import cyclonedds as rovosds
+sys.modules["rovosds"] = rovosds
+# ========================================================
+
+
+# ========================================================
+# --- SEKARANG ANDA BISA MENGGUNAKAN IMPORT DARI ROVOSDS ---
+# ========================================================
+from rovosds.domain import DomainParticipant
+from rovosds.idl import IdlStruct
+from rovosds.topic import Topic
+from rovosds.sub import Subscriber as DDSSubscriber, DataReader
+from rovosds.pub import Publisher as DDSPublisher, DataWriter
